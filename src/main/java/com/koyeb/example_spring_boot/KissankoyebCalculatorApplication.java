c1d29e81d4d690f66b2d4e0474fc3211d8fa314d
Bandarisgalaxy <harshithbandari810@gmail.com>
2025-02-05 15:03:41 +0530

    Initial commit

---
+++ src/main/java/com/koyeb/example_spring_boot/KissankoyebCalculatorApplication.java
@@ -0,0 +1,21 @@
+package com.koyeb.example_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KissankoyebCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KissankoyebCalculatorApplication.class, args);
+	}
+	@GetMapping("/")
+	public String hello()
+	{
+		return String.format("Hello world")	;
+	}
+
+}
---
+++ src/test/java/com/koyeb/example_spring_boot/KissankoyebCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koyeb.example_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KissankoyebCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
